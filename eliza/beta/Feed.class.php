1d23e286d7f5d69d70bd731a04723d8dd5ce16ad
sebataz <sebastien.rigoni@gmail.com>
2014-05-21 21:09:29 +0200

    filtered Feed constructor

--- eliza/beta/Feed.class.php
+++ eliza/beta/Feed.class.php
@@ -6,6 +6,11 @@ abstract class Feed extends Object {
     public function __get($_prop){ oops(OOPS); }
     public function __set($_prop, $_val) { oops(OOPS); }
     
+    public function __construct(array $array = array()) {
+        foreach (get_class_vars(get_called_class()) as $key => $default)
+            $this->$key = array_key_exists($key, $array) ? $array[$key] : $default;
+    }
+    
     public static function __callStatic($_feed, $_args) {
         self::load($_feed);
         return $_feed::__Feed($_args);
