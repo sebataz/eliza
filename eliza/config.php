c4aa5ac734eea15b7a1f9bf6bc03bee81f2ea7c8
sebataz <sebastien.rigoni@gmail.com>
2014-09-23 14:25:25 +0200

    removing stuff, preparing release

--- eliza/config.php
+++ eliza/config.php
@@ -1,11 +1,3 @@
 <?php
-$cfg['Title'] = 'sebataz.ch';
-
-$cfg['Me']['home'] = '.';
-$cfg['Me']['about me'] = "http://sebataz.ch/eliza";
-$cfg['Me']['curriculum'] = "https://careers.stackoverflow.com/rigoni";
-
-$cfg['Comunities']['github.com'] = 'https://github.com/sebataz';
-$cfg['Comunities']['sourceforge.net'] = 'https://sourceforge.net/users/sebataz';
-
-$cfg['Lock']['sebataz'] = 'test1234';
+$cfg['Title'] = 'eliza';
+$cfg['Lock']['root'] = 'root1234';
