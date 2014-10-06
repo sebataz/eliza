2dee4f9240ae004aec4c45875e7710d7f76584cc
sebataz <me@sebataz.ch>
2014-10-06 17:50:13 +0200

    new design for eliza response

--- eliza/index.php
+++ eliza/index.php
@@ -11,28 +11,50 @@ try {
     $feed = class_exists(key($_GET)) ? key($_GET) : null;
     $args = $feed ? array_slice($_GET, 1) : array();
     
+    
+    
+//----------------------------------------------------------------------------//
+//                                method: get                                 //
+//----------------------------------------------------------------------------//  
+    if (!$feed)
+        $Feed = new eliza\feed\HTMLFeed();
+    else
+        $Feed = eliza\beta\Response::Feed($feed, $args)
+            ->Q(isset($_GET['q']) ? $_GET['q'] : false)
+            ->filterBy(
+                isset($_GET['by']) ? $_GET['by'] : false,
+                isset($_GET['val']) ? $_GET['val'] : null
+            )
+            ->sortBy(
+                isset($_GET['srt']) ? $_GET['srt'] : null,
+                isset($_GET['ord']) ? $_GET['ord'] : null
+            )
+            ->limit(
+                isset($_GET['lmt']) ? $_GET['lmt'] : null,
+                isset($_GET['off']) ? $_GET['off'] : null
+            );
+            
+    if ($Feed->count() < 1)
+        $Feed->append($feed ? new $feed() : new eliza\beta\Object());
+     
+     
 
 //----------------------------------------------------------------------------//
 //                                method: post                                //
 //----------------------------------------------------------------------------//
-    if (count ($_POST)) {
-        if (eliza\beta\Response::hasPrivilege()) {
-            if ($feed) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (eliza\beta\Response::hasPrivilege() && $feed) {
+            
+            if (count($_POST) < 1) {
+                eliza\beta\Utils::deleteFile(
+                    eliza\beta\GlobalContext::Configuration()->Feed->Location
+                    . strtolower($feed) . DS 
+                    . $Feed->first()->Id . '.xml'
+                );
                 
-                $Feed = new eliza\feed\HTMLFeed(array(new $feed()));
-                
-                if (isset($_POST['Id'])) {
-                    
-                    $ToOverwrite = eliza\beta\Response::feed($feed)
-                        ->filterBy('Id', $_POST['Id']);
-                        
-                    if ($ToOverwrite->count())
-                        $Feed->first()->mergeWith(
-                            $ToOverwrite->first()->toArray()
-                        );
-                    
-                }
-                
+                header('Location: '. $_SERVER['HTTP_ORIGIN']);
+            
+            } else {
                 $Feed->first()->mergeWith($_POST);
                 
                 eliza\beta\Utils::writeFile(
@@ -42,44 +64,25 @@ try {
                     $Feed->XMLFeed()
                 );
             }
+            
         }
-            
-        if (eliza\beta\GlobalContext::Globals()->Get->defaultValue('redirect'))
-            header('Location: '. $_SERVER['HTTP_REFERER']);
-            
-        header('Content-Type: application/json');
-        echo '{"feed":' . $Feed->JSONFeed() . ',';
-        echo '"html":' . json_encode($Feed->HTMLFeed()) . '}';
-        
-        die();
     }
     
-        
     
+
 //----------------------------------------------------------------------------//
-//                                method: get                                 //
-//----------------------------------------------------------------------------//       
-    if (!$feed) die('none');
+//                                  always                                    //
+//----------------------------------------------------------------------------//      
+    if ($feed 
+    && !eliza\beta\GlobalContext::Globals()->Get->defaultValue('redirect')) {
+        header('Content-Type: application/json');     
+        echo '{"feed":' . $Feed->JSONFeed() . ',';
+        echo '"html":' . json_encode($Feed->HTMLFeed()) . '}';
     
-    $Feed = eliza\beta\Response::Feed($feed, $args)
-        ->Q(isset($_GET['q']) ? $_GET['q'] : false)
-        ->filterBy(
-            isset($_GET['by']) ? $_GET['by'] : false,
-            isset($_GET['val']) ? $_GET['val'] : null
-        )
-        ->sortBy(
-            isset($_GET['srt']) ? $_GET['srt'] : null,
-            isset($_GET['ord']) ? $_GET['ord'] : null
-        )
-        ->limit(
-            isset($_GET['lmt']) ? $_GET['lmt'] : null,
-            isset($_GET['off']) ? $_GET['off'] : null
-        );
-     
-    header('Content-Type: application/json');     
-    echo '{"feed":' . $Feed->JSONFeed() . ',';
-    echo '"html":' . json_encode($Feed->HTMLFeed()) . '}';
-            
+    } else header('Location: '. $_SERVER['HTTP_REFERER']);
+      
+
+      
 //----------------------------------------------------------------------------//
 //                                  fallback                                  //
 //----------------------------------------------------------------------------//  
