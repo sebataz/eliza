288b413e68bf69c5cdb578254d43960af9cfb656
sebataz <me@sebataz.ch>
2014-10-08 22:15:06 +0200

    better xml conversion

--- eliza/beta/XMLFeed.class.php
+++ eliza/beta/XMLFeed.class.php
@@ -8,18 +8,43 @@ interface XMLFeedI {
 
 class XMLFeed extends JSONFeed {
     public function XMLFeed() {
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-        
-        foreach ($this as $Object) {
-            $xml .= "\n" . '<' . strtolower(get_class($Object)) . '>' . "\n";
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>'
+            . self::CollectionToXML($this);
             
-            if ($Object instanceof XMLFeedI) $xml .= $Object->toXML();
+        return $xml;
+    }
+    
+    final public static function ObjectToXML($_Object) {
+        $xml = "\n";
+    
+        foreach ($_Object as $prop => $value) {
+            if ($value instanceof \eliza\beta\Object)
+                $xml .= "\t" . '<' . strtolower($prop) . '>'
+                    . self::ObjectToXML($value)
+                    . '</' . strtolower($prop) . '>' . "\n";
+            elseif ($value instanceof \eliza\beta\Collection)
+                $xml .= "\t" . '<' . strtolower($prop) . '>'
+                    . self::CollectionToXML($value)
+                    . '</' . strtolower($prop) . '>' . "\n";
+            elseif (is_array($value)) {
+                foreach ($value as $val)
+                    $xml .= "\t" . '<' . strtolower($prop) . '><![CDATA['
+                        . $value . ']]></' . strtolower($prop) . '>' . "\n";
+            }
             else
-                foreach ($Object as $prop => $value)
-                    if (!is_array($value) && !is_object($value))
-                        $xml .= "\t" . '<' . strtolower($prop) . '><![CDATA['
-                              . $value . ']]></' . strtolower($prop) . '>' . "\n";
-                
+                $xml .= "\t" . '<' . strtolower($prop) . '><![CDATA['
+                    . $value . ']]></' . strtolower($prop) . '>' . "\n";
+        }
+        
+        return $xml;
+    }
+    
+    final public static function CollectionToXML($_Collection) {
+        $xml = "\n";
+        
+        foreach ($_Collection as $Object) {
+            $xml .= "\n" . '<' . strtolower(get_class($Object)) . '>' . "\n";
+            $xml .= self::ObjectToXML($Object);
             $xml .= '</' . strtolower(get_class($Object)) . '>' . "\n";
         }
         
--- eliza/config.php
+++ eliza/config.php
@@ -10,3 +10,12 @@ $cfg['Feed']['LocationArticle'] = $cfg['Feed']['Location'] . 'article' . DS;
 $cfg['Feed']['LocationPage'] = $cfg['Feed']['Location'] . 'page' . DS;
 
 // Custom settings
+$cfg['Me']['blog'] = 'http://sebataz.ch/';
+$cfg['Me']['about me'] = "http://sebataz.ch/seba/";
+$cfg['Me']['curriculum'] = "https://careers.stackoverflow.com/rigoni";
+
+$cfg['Projects']['eliza'] = "http://eliza.sebataz.ch/";
+
+$cfg['Comunities']['github.com'] = 'https://github.com/sebataz';
+$cfg['Comunities']['sourceforge.net'] = 'https://sourceforge.net/users/sebataz';
+$cfg['Comunities']['stackoverflow.com'] = 'http://stackoverflow.com/users/935075/sebataz';
