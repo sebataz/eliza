7f1addfee05fe163c7bf44d80e33f4771a8b7162
sebataz <sebastien.rigoni@gmail.com>
2014-10-16 10:38:37 +0200

    bug fix in config

--- eliza/config.php
+++ eliza/config.php
@@ -12,13 +12,13 @@ $cfg['Feed']['LocationPage'] = $cfg['Feed']['Location'] . 'page' . DS;
 // Response settings
 $cfg['XMLResponse'] = false;
 
-$cfg['RSSFeed']['title'] = 'sebataz.ch'
+$cfg['RSSFeed']['title'] = 'sebataz.ch';
 $cfg['RSSFeed']['link'] = 'http://sebataz.ch';
-$cfg['RSSFeed']['description'] = 'sebataz\' blog'
-$cfg['RSSFeed']['language'] = 'en'                                                  // optional
-$cfg['RSSFeed']['image_title'] = 'sebataz.ch'                                       // optional
-$cfg['RSSFeed']['image_link'] = 'http://sebataz.ch'                                 // optional
-$cfg['RSSFeed']['image_url'] = 'http://sebataz.ch/eliza/public/img/sebataz.jpg'     // optional
+$cfg['RSSFeed']['description'] = 'sebataz\' blog';
+$cfg['RSSFeed']['language'] = 'en';                                                  // optional
+$cfg['RSSFeed']['image_title'] = 'sebataz.ch';                                       // optional
+$cfg['RSSFeed']['image_link'] = 'http://sebataz.ch';                                 // optional
+$cfg['RSSFeed']['image_url'] = 'http://sebataz.ch/eliza/public/img/sebataz.jpg';     // optional
 
 // Custom settings
 
